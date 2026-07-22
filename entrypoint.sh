3ae71c7e2f08a6c3dacea6259e07fcebb9681f16
SEY-JUGGLER <seyeelhadjimor0@gmail.com>
2026-07-22 07:02:15 +0000

    reglage de problemes de seeders

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,21 +1,17 @@
 #!/bin/sh
 
-echo "Configuration Laravel..."
+echo "Configuration Laravel (DEV)..."
 
 php artisan key:generate --force
 
 php artisan storage:link || true
 
-php artisan migrate --force
-
-php artisan db:seed --force
+# ⚠️ Supprime toutes les tables et recrée la base avant de lancer les seeders
+php artisan migrate:fresh --seed --force
 
 php artisan config:cache
-
 php artisan route:cache
-
 php artisan view:cache
-
 php artisan optimize
 
 php artisan serve --host=0.0.0.0 --port=$PORT
